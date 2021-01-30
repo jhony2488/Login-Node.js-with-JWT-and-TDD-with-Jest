4095fd3d4d90dc7ff29f9e541135b640b6581131
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-01-30 14:41:55 -0300

    reconfig jest and create .env files

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,5 @@
 const express = require('express')
-const { User } = require('./app/models')
 
 const routes = express.Router()
-
-User.create({
-    name: 'Jhonata Vinicius',
-    email: 'jhon.araujo2488@gmail.com',
-    password_hash: '65675675675656756757575',
-})
-    .then((user) => {
-        console.log('success', user.toJSON())
-    })
-    .catch((error) => {
-        console.log(error)
-    })
 
 module.exports = routes
---
+++ src/tests/integration/session.spec.js
@@ -0,0 +1,10 @@
+describe('Authentication', () => {
+    it('should sun two numbers', () => {
+        const x = 20
+        const y = 30
+
+        let sum = x + y
+
+        expect(sum).toBe(51)
+    })
+})
